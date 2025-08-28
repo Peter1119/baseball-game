5e7c29a25f520317ae27053fc8faf98aadc4f68b
홍석현 <ghdtjrgus333@naver.com>
2025-08-28 14:52:51 +0900

    chor.

--- BaseballGameApp/BaseballGameApp/GameManager.swift
+++ BaseballGameApp/BaseballGameApp/GameManager.swift
@@ -40,7 +40,7 @@ public class GameManager {
             guard let input = readLine(),
                   let state = GameState(rawValue: input)
             else {
-                print("올바르지 않은 입력입니다.")
+                print("올바르지 않은 입력입니다.\n")
                 continue
             }
             
@@ -56,10 +56,10 @@ public class GameManager {
                 }
             case .showRecords:
                 currenctState = .showRecords
-                print("기록을 보여주겠습니다.")
+                print("기록을 보여주겠습니다.\n")
             case .quit:
                 currenctState = .quit
-                print("종료하겠습니다.")
+                print("종료하겠습니다.\n")
                 break
             }
         }
